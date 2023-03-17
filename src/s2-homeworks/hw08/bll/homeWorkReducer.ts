ce07f27934b6bb796e04f7812812cb0e01fff3d0
Yaroslav Myratov <yaroslav.muratov.97@inbox.ru>
2023-03-17 14:42:45 +0300

    change

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -7,26 +7,36 @@ type ActionType =
 export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-             switch (action.payload){ case "up": {
-                 const collatore = new Intl.Collator('ru-RU')
-                let editState = state.sort((a,b)=>collatore.compare(a.name,b.name))
-                 let finalEdit = editState.map(el=>el)
-                 return finalEdit}
-             }
-            switch (action.payload){ case "down": {
-                const collatore = new Intl.Collator('ru-RU')
-                let editState = state.sort((a,b)=>collatore.compare(b.name,a.name))
-                            let finalEdit = editState.map(el=>el)
-                return finalEdit
-               }
-            }
-                 return state // need to fix
+            const newState = [...state].sort((a, b) => {
+                if (a.name > b.name) return 1
+                else if (a.name < b.name) return -1
+                else return 0
+            })
+            return action.payload === 'up' ? newState : newState.reverse()
         }
         case 'check': {
 
-              return state.filter(el => el.age > action.payload) // need to fix
+            return state.filter(el => el.age > action.payload) // need to fix
         }
+
         default:
             return state
     }
 }
+/*
+        case "check": {
+            const collatore = new Intl.Collator('ru-RU')
+
+            let editState = state.sort((a, b) => collatore.compare(b.name, a.name))
+
+            let finalEdit = editState.map(el => el)
+
+            return finalEdit
+        }*/
+
+/*const collatore = new Intl.Collator('ru-RU')
+         let editState = state.sort((a, b) => collatore.compare(a.name, b.name))
+
+         let finalEdit = editState.map(el => el)
+
+         return finalEdit*/
