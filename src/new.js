52e74d8c1268a086ef2a8fcc944cb853fb49daa6
Yaroslav Myratov <yaroslav.muratov.97@inbox.ru>
2023-03-17 13:27:38 +0300

    change

---
+++ src/new.js
@@ -0,0 +1,11 @@
+
+
+// let b = [{name: 'Кот'},
+//     {name: 'Аxa'},
+//     {name: 'Aaxa'}
+// ]
+let b = ['b','c','a']
+
+let c = b.sort(a-b)
+
+console.log(c)
--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,29 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
+             switch (action.payload){ case "up": {
+                 const collatore = new Intl.Collator('ru-RU')
+                let editState = state.sort((a,b)=>collatore.compare(a.name,b.name))
 
-            return state // need to fix
+                 return editState}
+             }
+            switch (action.payload){ case "down": {
+                const collatore = new Intl.Collator('ru-RU')
+                let editState = state.sort((a,b)=>collatore.compare(b.name,a.name))
+
+                return editState
+               }
+            }
+               // need to fix
         }
         case 'check': {
+switch (action.payload){
 
-            return state // need to fix
+}
+            return state.filter(el => el.age > 18) // need to fix
         }
         default:
             return state
